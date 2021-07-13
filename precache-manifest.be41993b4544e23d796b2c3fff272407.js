a82235b1a329b66751765301f23a432d9f3fa218
Serge Thompson <serge.thompson@gmail.com>
2021-07-13 16:14:49 -0400

    Updates

---
+++ precache-manifest.be41993b4544e23d796b2c3fff272407.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd4e05d91d4c4c054bced3b7586958c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f7bd052d7e751b81ee7a",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "34e6463651c4b17a7054",
+    "url": "./static/js/2.3c4e3000.chunk.js"
+  },
+  {
+    "revision": "8d2868001c00dcb0c4e1943da973e252",
+    "url": "./static/js/2.3c4e3000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7bd052d7e751b81ee7a",
+    "url": "./static/js/main.9b9a7b4a.chunk.js"
+  },
+  {
+    "revision": "fd6ffdc7d38c83f04ee9",
+    "url": "./static/js/runtime-main.e15ef3bc.js"
+  }
+]);
