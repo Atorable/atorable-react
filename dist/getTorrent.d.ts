c5c2c0b034ad9aa322c8ddd8583ab9075126b437
Serge Thompson <serge.thompson@gmail.com>
2022-01-07 22:45:12 -0500

    clean up

--- dist/getTorrent.d.ts
+++
@@ -1,8 +0,0 @@
-import WebTorrent, { Torrent } from 'webtorrent';
-export declare const GetTorrent: (magnetURI: string | {
-    default: string;
-}, manageTor: (file: WebTorrent.Torrent) => void) => void;
-export declare const loopThroughTorFiles: (torrent: Torrent, manageFile: (file: WebTorrent.TorrentFile) => void) => void;
-export declare const PromiseTorrent: (magnetURI: string | {
-    default: string;
-}) => Promise<WebTorrent.Torrent>;
